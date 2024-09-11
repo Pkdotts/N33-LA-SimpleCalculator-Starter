752aa3f01cf7bcb520d41b44f9e98caddf326633
Pkdotts <43733267+Pkdotts@users.noreply.github.com>
2024-09-11 18:30:07 -0400

    Added subtractions, multiplications and divisions. Also made it enter the operator again if it isn't valid.

--- SimpleCalculator/CalculatorEngine.cs
+++ SimpleCalculator/CalculatorEngine.cs
@@ -4,14 +4,40 @@ namespace SimpleCalculator
 {
     public class CalculatorEngine
     {
-        public double Calculate (string argOperation, double argFirstNumber, double argSecondNumber)
+        public double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
         {
             double result = 0;
 
-            if (argOperation == "+" || argOperation.ToLower() == "add") {
-                result = argFirstNumber + argSecondNumber;
-            }
+            bool yippie = false;
 
+            do
+            {
+                if (argOperation == "+" || argOperation.ToLower() == "add")
+                {
+                    result = argFirstNumber + argSecondNumber;
+                    yippie = true;
+                }
+                else if (argOperation == "-" || argOperation.ToLower() == "sub")
+                {
+                    result = argFirstNumber - argSecondNumber;
+                    yippie = true;
+                }
+                else if (argOperation == "*" || argOperation.ToLower() == "multiply")
+                {
+                    result = argFirstNumber * argSecondNumber;
+                    yippie = true;
+                }
+                else if (argOperation == "-" || argOperation.ToLower() == "divide")
+                {
+                    result = argFirstNumber / argSecondNumber;
+                    yippie = true;
+                }
+                else
+                {
+                    Console.WriteLine("Enter the Operator again (+, -, *, /)");
+                    argOperation = Console.ReadLine();
+                }
+            } while (!yippie);
 
             return result;
         }
