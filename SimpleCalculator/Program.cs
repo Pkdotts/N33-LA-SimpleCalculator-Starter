28ee2cf404de8dc5f86aac09242c504f8348992e
jeremy <jurassicjey@gmail.com>
2024-09-11 23:25:01 -0400

    Added formatted text and stringbuilder to properly show result. Also added text to indicate user to enter operation

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace SimpleCalculator
 {
@@ -11,13 +12,35 @@ namespace SimpleCalculator
                 // Class to perform actual calculations
                 CalculatorEngine calculatorEngine = new CalculatorEngine();
 
+
                 double firstNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine());
                 double secondNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine());
+                Console.WriteLine("enter your operation (add, subtract, divide, multiply");
                 string operation = Console.ReadLine();
                 
                 double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
-                
-                Console.WriteLine(result);
+
+                StringBuilder build = new StringBuilder();
+
+                if (operation == "+" || operation.ToLower() == "add")
+                {
+                    build.AppendFormat("The value {0:F2} plus the value {1:F2} is equal to {2:F2}", firstNumber, secondNumber, result);
+                }
+                else if (operation == "-" || operation.ToLower() == "minus")
+                {
+                    build.AppendFormat("The value {0:F2} minus the value {1:F2} is equal to {2:F2}", firstNumber, secondNumber, result);
+                }
+                else if (operation == "*" || operation.ToLower() == "times")
+                {
+                    build.AppendFormat("The value {0:F2} times the value {1:F2} is equal to {2:F2}", firstNumber, secondNumber, result);
+                }
+                else if (operation == "/" || operation.ToLower() == "divided")
+                {
+                    build.AppendFormat("The value {0:F2} divided by the value {1:F2} is equal to {2:F2}", firstNumber, secondNumber, result);
+                }
+
+
+                Console.WriteLine(build.ToString());
                 Console.WriteLine("Press Enter to End.");
                 string haha = Console.ReadLine();
             } catch (Exception ex)
